9ebaf187c78f66b87ee7b272d4d392da629b6fa3
thwjd12333 <thwjd12333@naver.com>
2025-10-28 15:25:57 +0900

    firs

---
+++ index.ts
@@ -0,0 +1,25 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
+
+import {TextLoader} from "langchain/document_loaders/fs/text";
+import { RecursiveCharaterTextSplitter} from "langchain/text_splitter";
+import {OpenAIEmbeddings} from "@langchain/openai"
+import {MemoryVectorStore} from "langchain/vectorsotres/memory";
+
+async function basicRagPipeline() {
+    console.log("RAG PipeLine Start");
+
+    const loader =new TextLoader("./sample.txt");
+    const docs = await loader.load();
+
+    console.log("1. Document Load Success")
+
+    const splitter = new RecursiveCharaterTextSplitter({
+        chunkSize: 200,
+        chunkOverlap: 20,
+    })
+
+    const splitDocs = await splitter.splitDocuments(docs);
+    console.log("2. Document Split Success");
+}
